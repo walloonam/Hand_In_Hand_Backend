807605c72698f4e2119c46f1c8421ecf9f4d926c
seoyeseon <dptjs6476@gmail.com>
2023-08-04 00:02:25 +0900

    post.views.py와urls.py 작성

--- handtohand/post/urls.py
+++ handtohand/post/urls.py
@@ -6,9 +6,11 @@ urlpatterns = [
     path('area/create/', views.create_area, name='area-create'),
     path('area/<int:pk>/delete/', views.delete_area, name='area-delete'),
     path('create/', views.create_post, name='create_post'),
-    path('<int:post_id>/', views.post_detail, name='post_detail'),
-    path('update/<int:id>/', views.update_post, name='update_post'),
-    path('delete/<int:id>/', views.delete_post, name='delete_post'),
-    path('list/', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),
+    path('<id:pk>/', views.post_detail, name='post_detail'),
+
+    # path('update/<int:id>/', views.update_post, name='update_post'),
+    # path('delete/<int:id>/', views.delete_post, name='delete_post'),
+
 
 ]
--- handtohand/post/views.py
+++ handtohand/post/views.py
@@ -25,42 +25,43 @@ def delete_area(request, pk): # 지역 삭제
 
 def create_post(request):
     if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save()
-            return redirect(' post_detail', post.id)
-        else:
-            return render(request, 'create_post.html')
-    else:
-        return render(request, 'create_post.html')
-#
-def post_detail(request):
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        point = request.POST.get('point')
+    return JsonResponse({'message': '작성 성공'})
+
+def post_list(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        area.name = data["name"]
+        posts = list(Post.objects.filter(name__contains=name).values("name"))
+        context = {
+            "posts": posts
+        }
+        return JsonResponse(context,{
+            "id": 1,
+			"title" : "도와주세요",
+			"content" : "배고파여",
+			"point" : 500,
+			"numChat" : 5,
+			"area" : "동작구"
+        })
+
+
+def post_detail(request,post_id):
     post = Post.objects.get(id=post_id)
     post.save()
-    #채팅하는 사람의 수..
     context = {
         "post": post,
-        #채팅하는 사람의 수
     }
-    return render(request, "post_detail.html", context)
-#
-def update_post(request):
-    post = get_object_or_404(Job_post, id=id)
-    if request.method == 'POST':
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            form.save()
-            return redirect('post_detail', post.id)
-        else:
-            return render(request, 'create_post.html')
+    return JsonResponse(context,{
+        "id" : "1",
+        "title": "배고파요",
+        "content": "안녕하세여",
+        "numChat": 5,
+        "point": 500,
+        "created_at" : "2023-07-21T23:00:50Z",
+        "area" : "동작구"
+    })
 
-    else:
-        return render(request, 'create_post.html', {"post": post})
-#
-def delete_post(request,id):
-    post = get_object_or_404(Post, id=id)
-    post.delete()
-    return redirect('post_list')
-
-def post_list(request): #ajax?
-    posts = list(Post.objects.all().order_by("-created_at").values("id", "title", "content")) #채팅자 수
+    # post.objects.filter(id)
