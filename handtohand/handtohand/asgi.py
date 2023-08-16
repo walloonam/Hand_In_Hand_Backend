e92dc5a21c301804095bad8a09e93705afb98b75
JaeHun Leem <80560040+walloonam@users.noreply.github.com>
2023-08-16 19:45:44 +0900

    채팅 개선 3

---
+++ handtohand/handtohand/asgi.py
@@ -0,0 +1,16 @@
+import os
+
+import django
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+application = ProtocolTypeRouter(
+  {
+    "http": get_asgi_application(),
+# Just HTTP for now. (We can add other protocols later.)
+  }
+)
