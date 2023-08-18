f629f102f5f3f7094dc2805ec288bc87b546be21
JaeHun Leem <80560040+walloonam@users.noreply.github.com>
2023-08-18 20:38:16 +0900

    채팅 시작10

--- handtohand/chatting/consumers.py
+++ handtohand/chatting/consumers.py
@@ -22,7 +22,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def disconnect(self, close_code):
         # Leave room group
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        await self.channel_layer.group_discard(self.channel_name)
 
     # Receive message from WebSocket
     async def receive(self, text_data):
