073be3082464fe7d82a7659a25e28bd168241887
JaeHun Leem <80560040+walloonam@users.noreply.github.com>
2023-08-18 20:45:19 +0900

    채팅 시작14

--- handtohand/chatting/consumers.py
+++ handtohand/chatting/consumers.py
@@ -11,12 +11,11 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        #이친구둘은 없어도 될지도
         self.room_name = 'hi'
         self.room_group_name = f"chat_{self.room_name}"
 
         # Join room group
-        await self.channel_layer.group_add(self.room_group_name,self.channel_name)
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
         await self.accept()
 
@@ -35,13 +34,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
         room_id = text_data_json.get('room_id')
         user_id = text_data_json.get('user_id')
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                "content":content,
-                "room_id" : room_id,
-                "user_id" : user_id,
+                "content": content,
+                "room_id": room_id,
+                "user_id": user_id,
             }
         )
 
@@ -53,8 +52,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
         room_id = event.get('room_id')
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'content': content,
-            "user_id" : user_id,
-            "room_id" : room_id,
+            "user_id": user_id,
+            "room_id": room_id,
         }))
